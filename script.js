22ce49a1856d19cdbe6fcd6465db9234fb5246ef
umairansari92 <154614381+umairansari92@users.noreply.github.com>
2024-06-19 14:57:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-// Example JavaScript for form submission handling
-const form = document.querySelector('form');
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
-    // Additional logic for handling form data with AJAX, etc.
-    alert('Form submitted!'); // Example alert
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.getElementById('menu-icon');
-    const navLinks = document.getElementById('nav-links');
-
-    if (menuIcon) {
-        menuIcon.addEventListener('click', () => {
-            navLinks.classList.toggle('show');
-            const icon = menuIcon.querySelector('i');
-            if (icon.classList.contains('fa-bars')) {
-                icon.classList.remove('fa-bars');
-                icon.classList.add('fa-times');
-            } else {
-                icon.classList.remove('fa-times');
-                icon.classList.add('fa-bars');
-            }
-        });
-    }
-});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
